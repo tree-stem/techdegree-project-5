321db5f6087d0985ef8d0dd0c388fba358e0ee6e
Tristan Ringer <rin9er@gmail.com>
2025-01-01 13:31:13 -0700

    Completed steps 1 & 2

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -4,6 +4,7 @@ async function getPersonData() {
   const response = await fetch("https://randomuser.me/api/").then((response) =>
     response.json()
   );
+
   const results = response.results[0];
   const getFirstName = results.name["first"];
   const getLastName = results.name["last"];
@@ -11,6 +12,7 @@ async function getPersonData() {
   const getCity = results.location["city"];
   const getState = results.location["state"];
   const getPhoto = results.picture["large"];
+  const getPhone = results.phone;
 
   const person = {
     firstName: getFirstName,
@@ -19,6 +21,7 @@ async function getPersonData() {
     city: getCity,
     state: getState,
     picture: getPhoto,
+    phone: getPhone,
   };
 
   return person;
@@ -47,6 +50,37 @@ async function fetchPersonsData(count) {
       `;
 
     gallery.insertAdjacentHTML("beforeend", html);
+
+    const card = gallery.querySelector(".card:last-child");
+
+    card.addEventListener("click", () => {
+      const modal = document.createElement("div");
+
+      const modalHTML = `
+        <div class="modal-container">
+          <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+              <img class="modal-img" src="${person.picture}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${person.firstName} ${person.lastName}</h3>
+              <p class="modal-text">${person.email}</p>
+              <p class="modal-text cap">${person.city}</p>
+              <hr>
+              <p class="modal-text">${person.phone}</p>
+              <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+              <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>`;
+
+      modal.insertAdjacentHTML("beforeend", modalHTML);
+      gallery.appendChild(modal);
+
+      const closeButton = document.getElementById("modal-close-btn");
+
+      closeButton.addEventListener("click", (event) => {
+        gallery.removeChild(modal);
+      });
+    });
   });
   console.log(persons);
 }
