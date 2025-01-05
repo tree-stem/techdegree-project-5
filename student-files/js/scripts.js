f707cb4f457da4a7f901c20f14fb9310b95512b1
Tristan Ringer <rin9er@gmail.com>
2025-01-04 23:42:47 -0700

    Added search bar

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -4,26 +4,28 @@ const gallery = document.getElementById("gallery");
 // Created a function that makes a promise to return the users information
 async function getPersonsData(count) {
   const response = await fetch(
-    `https://randomuser.me/api/?results=${count}`
+    `https://randomuser.me/api/?results=${count}&nat=us`
   ).then((response) => response.json());
 
-  console.log(response.results);
   return response.results;
 }
 
 // Created a function that collects user data and displays it in html through interpolation
 async function displayPersonsData(count) {
-  const persons = await getPersonsData(count);
-  persons.map((person) => {
+  const userNames = [];
+  console.log(userNames);
+  const users = await getPersonsData(count);
+
+  users.map((user) => {
     const html = `
       <div class="card">
         <div class="card-img-container">
-          <img class="card-img" src="${person.picture.large}" alt="profile picture">
+          <img class="card-img" src="${user.picture.large}" alt="profile picture">
         </div>
         <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-          <p class="card-text">${person.email}</p>
-          <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="card-text">${user.email}</p>
+          <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
         </div>
       </div>
       `;
@@ -34,8 +36,7 @@ async function displayPersonsData(count) {
 
     card.addEventListener("click", () => {
       const modal = document.createElement("div");
-      const birthday = person.dob.date;
-      console.log(birthday);
+      const birthday = user.dob.date;
       const birthdayDisplay = `
       ${birthday.slice(5, 7)}/${birthday.slice(8, 10)}/${birthday.slice(0, 4)}
       `;
@@ -45,13 +46,13 @@ async function displayPersonsData(count) {
           <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-              <img class="modal-img" src="${person.picture.large}" alt="profile picture">
-              <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
-              <p class="modal-text">${person.email}</p>
-              <p class="modal-text cap">${person.location.city}</p>
+              <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+              <p class="modal-text">${user.email}</p>
+              <p class="modal-text cap">${user.location.city}</p>
               <hr>
-              <p class="modal-text">${person.cell}</p>
-              <p class="modal-text">${person.location.street["number"]} ${person.location.street["name"]}, ${person.location.state}, ${person.nat} ${person.location.postcode}</p>
+              <p class="modal-text">${user.cell}</p>
+              <p class="modal-text">${user.location.street["number"]} ${user.location.street["name"]}, ${user.location.state}, ${user.nat} ${user.location.postcode}</p>
               <p class="modal-text">Birthday: ${birthdayDisplay}</p>
             </div>
         </div>`;
@@ -63,6 +64,78 @@ async function displayPersonsData(count) {
 
       closeButton.addEventListener("click", (event) => {
         gallery.removeChild(modal);
+      });
+    });
+    const userFullName = `${user.name["first"]} ${user.name["last"]}`;
+    userNames.push(userFullName);
+  });
+  const namesLowerCase = userNames.map((name) => {
+    return name.toLowerCase();
+  });
+  handleSearch(namesLowerCase, userNames, users);
+}
+
+const searchInput = document.getElementById("search-input");
+
+function handleSearch(namesLowerCase, userNames, users) {
+  searchInput.addEventListener("input", () => {
+    const searchedName = searchInput.value.toLowerCase();
+
+    const filteredUsers = users.filter((user, index) =>
+      namesLowerCase[index].includes(searchedName)
+    );
+
+    gallery.innerHTML = "";
+
+    filteredUsers.forEach((user) => {
+      const html = `
+        <div class="card">
+          <div class="card-img-container">
+            <img class="card-img" src="${user.picture.large}" alt="profile picture">
+          </div>
+          <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+          </div>
+        </div>
+      `;
+
+      gallery.insertAdjacentHTML("beforeend", html);
+
+      const card = gallery.querySelector(".card:last-child");
+
+      card.addEventListener("click", () => {
+        const modal = document.createElement("div");
+        const birthday = user.dob.date;
+        const birthdayDisplay = `
+      ${birthday.slice(5, 7)}/${birthday.slice(8, 10)}/${birthday.slice(0, 4)}
+      `;
+
+        const modalHTML = `
+        <div class="modal-container">
+          <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+              <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+              <p class="modal-text">${user.email}</p>
+              <p class="modal-text cap">${user.location.city}</p>
+              <hr>
+              <p class="modal-text">${user.cell}</p>
+              <p class="modal-text">${user.location.street["number"]} ${user.location.street["name"]}, ${user.location.state}, ${user.nat} ${user.location.postcode}</p>
+              <p class="modal-text">Birthday: ${birthdayDisplay}</p>
+            </div>
+        </div>`;
+
+        modal.insertAdjacentHTML("beforeend", modalHTML);
+        gallery.appendChild(modal);
+
+        const closeButton = document.getElementById("modal-close-btn");
+
+        closeButton.addEventListener("click", (event) => {
+          gallery.removeChild(modal);
+        });
       });
     });
   });
