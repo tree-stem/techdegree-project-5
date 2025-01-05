7a06067acfcaeeae4172fa41c5aa1da8d2188617
Tristan Ringer <rin9er@gmail.com>
2025-01-05 09:56:47 -0700

    Updated event listeners on searchbar

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -76,19 +76,21 @@ async function displayPersonsData(count) {
 }
 
 const searchInput = document.getElementById("search-input");
+const searchSubmit = document.getElementById("search-submit");
 
 function handleSearch(namesLowerCase, userNames, users) {
-  searchInput.addEventListener("input", () => {
-    const searchedName = searchInput.value.toLowerCase();
+  searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const searchedName = searchInput.value.toLowerCase();
 
-    const filteredUsers = users.filter((user, index) =>
-      namesLowerCase[index].includes(searchedName)
-    );
+      const filteredUsers = users.filter((user, index) =>
+        namesLowerCase[index].includes(searchedName)
+      );
 
-    gallery.innerHTML = "";
+      gallery.innerHTML = "";
 
-    filteredUsers.forEach((user) => {
-      const html = `
+      filteredUsers.forEach((user) => {
+        const html = `
         <div class="card">
           <div class="card-img-container">
             <img class="card-img" src="${user.picture.large}" alt="profile picture">
@@ -101,18 +103,18 @@ function handleSearch(namesLowerCase, userNames, users) {
         </div>
       `;
 
-      gallery.insertAdjacentHTML("beforeend", html);
+        gallery.insertAdjacentHTML("beforeend", html);
 
-      const card = gallery.querySelector(".card:last-child");
+        const card = gallery.querySelector(".card:last-child");
 
-      card.addEventListener("click", () => {
-        const modal = document.createElement("div");
-        const birthday = user.dob.date;
-        const birthdayDisplay = `
+        card.addEventListener("click", () => {
+          const modal = document.createElement("div");
+          const birthday = user.dob.date;
+          const birthdayDisplay = `
       ${birthday.slice(5, 7)}/${birthday.slice(8, 10)}/${birthday.slice(0, 4)}
       `;
 
-        const modalHTML = `
+          const modalHTML = `
         <div class="modal-container">
           <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -127,6 +129,69 @@ function handleSearch(namesLowerCase, userNames, users) {
               <p class="modal-text">Birthday: ${birthdayDisplay}</p>
             </div>
         </div>`;
+
+          modal.insertAdjacentHTML("beforeend", modalHTML);
+          gallery.appendChild(modal);
+
+          const closeButton = document.getElementById("modal-close-btn");
+
+          closeButton.addEventListener("click", (event) => {
+            gallery.removeChild(modal);
+          });
+        });
+      });
+    }
+  });
+
+  searchSubmit.addEventListener("click", () => {
+    const searchedName = searchInput.value.toLowerCase();
+
+    const filteredUsers = users.filter((user, index) =>
+      namesLowerCase[index].includes(searchedName)
+    );
+
+    gallery.innerHTML = "";
+
+    filteredUsers.forEach((user) => {
+      const html = `
+      <div class="card">
+        <div class="card-img-container">
+          <img class="card-img" src="${user.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="card-text">${user.email}</p>
+          <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+        </div>
+      </div>
+    `;
+
+      gallery.insertAdjacentHTML("beforeend", html);
+
+      const card = gallery.querySelector(".card:last-child");
+
+      card.addEventListener("click", () => {
+        const modal = document.createElement("div");
+        const birthday = user.dob.date;
+        const birthdayDisplay = `
+    ${birthday.slice(5, 7)}/${birthday.slice(8, 10)}/${birthday.slice(0, 4)}
+    `;
+
+        const modalHTML = `
+      <div class="modal-container">
+        <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="modal-text">${user.email}</p>
+            <p class="modal-text cap">${user.location.city}</p>
+            <hr>
+            <p class="modal-text">${user.cell}</p>
+            <p class="modal-text">${user.location.street["number"]} ${user.location.street["name"]}, ${user.location.state}, ${user.nat} ${user.location.postcode}</p>
+            <p class="modal-text">Birthday: ${birthdayDisplay}</p>
+          </div>
+      </div>`;
 
         modal.insertAdjacentHTML("beforeend", modalHTML);
         gallery.appendChild(modal);
